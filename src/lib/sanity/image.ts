6e9f3231a49ff4aa752618b54669a3b6f8aa810e
padunk <abraham.agel@gmail.com>
2024-02-12 18:45:57 +0700

    update image component

--- src/lib/sanity/image.ts
+++ src/lib/sanity/image.ts
@@ -1,6 +1,5 @@
 import { sanityClient } from "sanity:client";
 
-/* lib/sanity/image.ts */
 import imageUrlBuilder from "@sanity/image-url";
 
 type ImageUrlBuilder = ReturnType<typeof imageUrlBuilder>;
